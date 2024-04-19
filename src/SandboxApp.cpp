c8855ca2d77e3525075c8b9a4f57789f2573b050
nehaprasad13 <84422116+nehaprasad13@users.noreply.github.com>
2024-04-19 17:10:43 -0400

    Update SandboxApp.cpp

--- src/SandboxApp.cpp
+++ src/SandboxApp.cpp
@@ -1,4 +1,13 @@
-#include "GLCore.h"
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <imgui.h>
+
+#include "GLCore/Core/Application.h"
+#include "GLCore/Core/Input.h"
+#include "GLCore/Core/KeyCodes.h"
+#include "GLCore/Core/MouseButtonCodes.h"
 #include "SandboxLayer.h"
 
 using namespace GLCore;
